63b44468ce63f729c565981234e8ba69e64a7a4f
Yen-Chieh Huang <lashhw81@gmail.com>
2024-07-22 14:19:44 +0800

    read .lib/.db/.v from /tmp instead of ../sram

--- work/lc.tcl
+++ work/lc.tcl
@@ -1,7 +1,7 @@
 #set LIB_NAME "rf_2p_hse_ccs_tt_0p90v_0p90v_25c"
 set LIB_NAME "sram_dp_hde_ccs_tt_0p90v_0p90v_25c"
-set LIB "../sram/${LIB_NAME}_syn.lib"
-set DB "../sram/${LIB_NAME}.db"
+set LIB "/tmp/${LIB_NAME}_syn.lib"
+set DB "/tmp/${LIB_NAME}.db"
 
 read_lib ${LIB}
 write_lib ${LIB_NAME} -format db -output ${DB}
